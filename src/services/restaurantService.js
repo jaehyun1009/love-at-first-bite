dbb59235e6cb6e70346fa25256c93c936298c604
jaehyun1009 <j.jaehuyn.1009@gmail.com>
2021-08-12 15:33:00 -0700

    implement api search

--- src/services/restaurantService.js
+++ src/services/restaurantService.js
@@ -1,2 +1,22 @@
 import * as tokenService from "./tokenService"
 const BASE_URL = "/api/restaurants/"
+
+export function search(location, category){
+  return fetch(
+    `${BASE_URL}search/${location}/${category}`,
+    {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` }
+    },{
+      mode: 'cors'
+    }).then(res => res.json())
+}
+
+export function searchWithoutCategory(location){
+  return fetch(
+    `${BASE_URL}search/${location}`,
+    {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` }
+    },{
+      mode: 'cors'
+    }).then(res => res.json())
+}
