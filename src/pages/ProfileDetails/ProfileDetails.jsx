e5b5fdec796e3a1d5f8aa82943841ad20e338570
jaehyun1009 <j.jaehuyn.1009@gmail.com>
2021-08-13 18:34:55 -0700

    add and remove restaurants on profile page

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 import DisplayName from '../../components/DisplayName/DisplayName'
+import RestaurantForm from '../../components/RestaurantForm/RestaurantForm'
 
-const ProfileDetails = ({location, userProfile}) => {
+const ProfileDetails = ({location, userProfile, handleAddRestaurant, handleRemoveRestaurant}) => {
   const {profile} = location.state
   return (
     <>
       <h1><DisplayName profile={profile}/></h1>
       <h2>Favorite Restaurants</h2>
       {
-        location.state.profile.restaurants.map((restaurant, idx) => 
+        location.state.profile.restaurants.map(restaurant => 
           <div key={restaurant.id}>
             <h5>{restaurant.name}</h5>
             <a href={`/restaurants/${restaurant.id}`}>
               <img src={restaurant.img_url} width='200px' alt="restaurant"/>
             </a>
+            {
+              (profile._id === userProfile._id) && 
+              <RestaurantForm
+                restaurant={restaurant}
+                userProfile={userProfile}
+                handleAddRestaurant={handleAddRestaurant}
+                handleRemoveRestaurant={handleRemoveRestaurant}
+              />
+            }
           </div>
         )
       }
