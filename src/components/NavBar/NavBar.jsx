2503559968501c3e79570394313c0e9f3d8523cf
btsybm <betsybeam2@gmail.com>
2021-08-16 11:26:46 -0500

    css modules

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,29 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import SearchForm from '../SearchForm/SearchForm'
 import ProfileCard from '../ProfileCard/ProfileCard'
-import styles from './NavBar.css'
+import styles from './NavBar.module.css'
 
 const NavBar = ({ userProfile, handleLogout, history }) => {
 	const profile = userProfile
 	return (
 		<>
 			{userProfile ? (
-				<nav className='menu'>
+				<nav className={styles.menu}>
 						<ol>
-							<li className='menu-item'><Link to="/">HOME</Link></li>
-							<li className='menu-item'><Link to={{ pathname: "/profile", state: {profile}}} >MY PROFILE</Link></li>
-							<li className='menu-item'><Link to='' onClick={handleLogout}>LOG OUT</Link></li>
-							<li className='menu-item'><Link to="/users">Users(Temp)</Link></li>
+							<li><Link to="/">HOME</Link></li>
+							<li><Link to={{ pathname: "/profile", state: {profile}}} >MY PROFILE</Link></li>
+							<li><Link to='' onClick={handleLogout}>LOG OUT</Link></li>
+							<li><Link to="/users">Users(Temp)</Link></li>
 							<li> <Link to='/messages'>messages</Link></li>
 							{/* <SearchForm history={history} /> */}
 						</ol>
 				</nav>
 			) : (
-				<nav className='menu'>
+				<nav className={styles.menu}>
 						<ol>
-							<li className='menu-item'><Link to="/login">LOG IN</Link></li>
-							<li className='menu-item'><Link to="/signup">SIGN UP</Link></li>
-							<li className='menu-item'><Link to="/users">Users(Temp)</Link></li>
+							<li><Link to="/login">LOG IN</Link></li>
+							<li><Link to="/signup">SIGN UP</Link></li>
+							<li><Link to="/users">Users(Temp)</Link></li>
 						</ol>
 				</nav>
 			)}
