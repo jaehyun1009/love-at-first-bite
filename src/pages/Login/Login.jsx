9abb767c2722598ea9849a7b48a30609ce7e9f6d
btsybm <betsybeam2@gmail.com>
2021-08-16 12:03:50 -0500

    login form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -7,9 +7,8 @@ class LoginPage extends Component {
   render() {
     return (
       <main className={styles.container}>
-        <h1>Log In</h1>
         <LoginForm history={this.props.history} handleSignupOrLogin={this.props.handleSignupOrLogin}/>
-        <Link to="/signup"> Not a member, sign up here. </Link>
+
       </main>
     )
   }
