9c3daccaee65f384b4e3361d907ae2dfff8aec5c
btsybm <betsybeam2@gmail.com>
2021-08-16 10:21:54 -0500

    favorite restaurant component and css

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -1,34 +1,19 @@
 import React from 'react';
 import RestaurantForm from '../../components/RestaurantForm/RestaurantForm'
+import styles from './ProfileDetails.module.css'
+import FavoriteRestaurants from '../../components/FavoriteRestaurants/FavoriteRestaurants'
+
 
 const ProfileDetails = ({location, userProfile, handleAddRestaurant, handleRemoveRestaurant}) => {
   const {profile} = location.state
   return (
     <>
-      <h1>{profile.firstName} {profile.lastName && profile.lastName + '.'}</h1>
-      <h2>Favorite Restaurants</h2>
-      {
-        userProfile && 
-        location.state.profile.restaurants.map(restaurant => 
-          <div key={restaurant.id}>
-            <h5>{restaurant.name}</h5>
-            <a href={`/restaurants/${restaurant.id}`}>
-              <img src={restaurant.img_url} width='200px' alt="restaurant"/>
-            </a>
-            {
-              (profile._id === userProfile._id) && 
-              <RestaurantForm
-                key={restaurant.id}
-                restaurant={restaurant}
-                userProfile={userProfile}
-                handleAddRestaurant={handleAddRestaurant}
-                handleRemoveRestaurant={handleRemoveRestaurant}
-              />
-            }
-          </div>
-        )
-      }
-      <h2>Messages</h2>
+    <FavoriteRestaurants 
+      profile= {profile}
+      userProfile= {userProfile}
+      handleAddRestaurant= {handleAddRestaurant}
+      handleRemoveRestaurant= {handleRemoveRestaurant}
+    />
     </>
   )
 }
