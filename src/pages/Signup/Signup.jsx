e867668dac4a62d0c43eeb8a0916859ab1760100
btsybm <betsybeam2@gmail.com>
2021-08-19 20:23:38 -0500

    header

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import SignupForm from '../../components/SignupForm/SignupForm'
 import styles from './Signup.module.css'
 import { Link } from 'react-router-dom'
+import Header from '../../components/Header/Header'
+
 
 
 class Signup extends Component {
@@ -15,10 +17,13 @@ class Signup extends Component {
 
   render() {
     return (
+      <>
+      <Header />
       <main className={styles.container}>
         <p>{this.state.message}</p>
         <SignupForm {...this.props} updateMessage={this.updateMessage} />
       </main>
+      </>
     )
   }
 }
