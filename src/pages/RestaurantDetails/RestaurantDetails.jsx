29c81994a6859143458d63bd580b5639f00a5580
btsybm <betsybeam2@gmail.com>
2021-08-20 01:07:50 -0500

    restaurant details when following

--- src/pages/RestaurantDetails/RestaurantDetails.jsx
+++ src/pages/RestaurantDetails/RestaurantDetails.jsx
@@ -21,34 +21,34 @@ class RestaurantDetails extends Component{
         {
           (searchResult._id) ? 
           <>
-            <h1 className={styles.header}>{searchResult?.name}</h1>
-            
-            
+            <h1 className={styles.header}><a href={searchResult?.url} target="blank"> {searchResult?.name}</a> </h1>
             <img className={styles.picture} src={searchResult?.img_url} width='300px' alt='business' />
             <br />
             <h3 className={styles.info}>Address: {searchResult?.address}</h3>
             <h3 className={styles.info}>Phone Number: {searchResult?.phone}</h3>
             <h3 className={styles.info}>Rating: {searchResult?.rating} ({searchResult?.review_count} Reviews) &emsp;&emsp; Price Rating: {searchResult?.price}</h3>
             <h3 className={styles.info}>Categories: {searchResult?.categories?.map(category => category.title).join(', ')}</h3>
-            <h3 className={styles.info}><a href={searchResult?.url}>Yelp link to business</a></h3>
+            {/* <h3 className={styles.info}><a href={searchResult?.url}>Yelp link to business</a></h3> */}
             { !!searchResult?.likedBy?.find(profile => profile._id === this.props.userProfile?._id) && 
               <>
-                <h2>Others who liked this restaurant</h2>
+                <h2 className={styles.otherPeople}>OTHERS WHO LIKE THIS RESTAURANT</h2>
+                <div className={styles.peopleList}>
                 { searchResult?.likedBy?.map(profile => {
                     if (profile._id !== this.props.userProfile?._id)
                       return <ProfileCard key={profile._id} profile={profile} />
                   })
                 }
+                </div>
               </>
             }
-            <RestaurantForm
+           <RestaurantForm
               key={searchResult?.id}
               restaurant={searchResult}
               userProfile={this.props.userProfile}
               handleAddRestaurant={this.props.handleAddRestaurant}
               handleRemoveRestaurant={this.props.handleRemoveRestaurant}
               className={styles.restaurantButton}
-            />
+            /> 
           </> : <>
             <h1 className={styles.header}>{searchResult?.name}</h1>
             <img className={styles.picture}  src={searchResult?.image_url} width='500px' alt='business'/>
